9d53f3daf4f94cd5a1157c730be876412daa5d6e
Alberto Muratore <a.muratore@pm.me>
2022-06-10 14:44:37 +0200

    added missing annotations

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello continous Integration and Delivery!";
 	}
 
 	public static void main(String[] args) {
